74c196d2de83ffe3b8b5bde47862fc5d871d6bc6
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-07-05 01:05:24 -0400

    distribute experienced players equally on all teams

--- league_builder.py
+++ league_builder.py
@@ -13,17 +13,43 @@ def read_csv_file():
 
 			players.append(player)
 
-	experpienced_players(players)
+	count_experpienced_players(players)
 
-def experpienced_players(players):
+def count_experpienced_players(players):
 
-	number_of_experienced_players = 0
+	#number_of_experienced_players = 0
+	experienced_players = []
+	non_experienced_players = []
 
 	for player in players:
 		if player['experience'] == 'YES':
-			number_of_experienced_players += 1
+			#number_of_experienced_players += 1
+			experienced_players.append(player)
+		else:
+			non_experienced_players.append(player)
 
-	print(number_of_experienced_players)
+	#print(number_of_experienced_players)
+	divide_players(experienced_players, non_experienced_players, players)
+
+def divide_players(experienced_players, non_experienced_players, players):
+
+	NUMBER_OF_TEAMS = 3
+	number_of_players_per_team = int(len(players) / NUMBER_OF_TEAMS)
+	experienced_players_per_team = int(len(experienced_players) / NUMBER_OF_TEAMS)
+
+	#print(number_of_players_per_team)
+	#print(experienced_players_per_team)
+
+	sharks = experienced_players[0:experienced_players_per_team]
+	dragons = experienced_players[experienced_players_per_team: experienced_players_per_team*2]
+	raptors = experienced_players[experienced_players_per_team*2:]
+
+	print(sharks)
+	print('---------------------------------------')
+	print(dragons)
+	print('---------------------------------------')
+	print(raptors)
+
 
 
 
