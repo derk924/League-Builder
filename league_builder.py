fb9c2ea41f4f926db4f839c6a210983318efbd78
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-07-04 23:50:01 -0400

    read from csv file

---
+++ league_builder.py
@@ -0,0 +1,17 @@
+import csv
+
+def read_csv_file():
+	players = []
+	with open('soccer_players.csv') as file_object:
+		rows = csv.DictReader(file_object, delimiter = ',')
+		for row in rows:
+			player = {'name':'', 'height':'', 'experience':'', 'guardian':''}
+			player['name'] = row['Name']
+			player['height'] = row['Height (inches)']
+			player['experience'] = row['Soccer Experience']
+			player['guardian'] = row['Guardian Name(s)']
+
+			players.append(player)
+
+
+read_csv_file() 
