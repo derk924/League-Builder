91115650cc9caff718918479f3241700c6935ed3
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-07-06 23:51:52 -0400

    add comments

--- league_builder.py
+++ league_builder.py
@@ -1,20 +1,28 @@
 import csv
 
-def read_csv_file():
-	players = []
-	with open('soccer_players.csv') as file_object:
-		rows = csv.DictReader(file_object, delimiter = ',')
-		for row in rows:
-			player = {'name':'', 'height':'', 'experience':'', 'guardian':''}
-			player['name'] = row['Name']
-			player['height'] = row['Height (inches)']
-			player['experience'] = row['Soccer Experience']
-			player['guardian'] = row['Guardian Name(s)']
+# function below is for reading form CSV file and then assign data 
+# extracted to players list
 
-			players.append(player)
+def read_csv_file(file_object):
+	players = [] # a list for all players
+	
+	rows = csv.DictReader(file_object, delimiter = ',')
+	for row in rows:
+		player = {'name':'', 'height':'', 'experience':'', 'guardian':''}
+		player['name'] = row['Name']
+		player['height'] = row['Height (inches)']
+		player['experience'] = row['Soccer Experience']
+		player['guardian'] = row['Guardian Name(s)']
+
+		players.append(player)
 
 	count_experpienced_players(players)
 
+#---------------------------------------------------------------------------------#
+
+# function below receives a list of all players and then finds the experienced players
+# and adds them to a new list called experienced_players. Also if a player does not have
+# then it will be added to the non_experienced_players list
 
 def count_experpienced_players(players):
 
@@ -29,13 +37,10 @@ def count_experpienced_players(players):
 		else:
 			non_experienced_players.append(player)
 
-	#print(experienced_players)
-	#print('-----------------------------')
-	#print(non_experienced_players)
-
-	#print(number_of_experienced_players)
 	divide_players(experienced_players, non_experienced_players, players)
 
+#---------------------------------------------------------------------------------#
+#function below divide players equally and adds then to three lists (sharks, dragons and raptors)
 
 def divide_players(experienced_players, non_experienced_players, players):
 
@@ -58,11 +63,14 @@ def divide_players(experienced_players, non_experienced_players, players):
 
 	save_to_file(sharks,dragons,raptors)
 
+#---------------------------------------------------------------------------------#
+#function below saves information about players in team.txt file
+
 def save_to_file(sharks, dragons, raptors):
 
 	with open('teams.txt', 'a') as file:
-		file.seek(0)
-		file.truncate()
+		file.seek(0) # start from the begining of the files
+		file.truncate() # remove all data from a file
 
 		file.write('Sharks Team:\n============\n\n')
 		for player in sharks:
@@ -84,14 +92,16 @@ def save_to_file(sharks, dragons, raptors):
 	all_players = sharks + dragons + raptors
 	create_welcome_letters(all_players, sharks, dragons, raptors)
 
+#---------------------------------------------------------------------------------#
+# function below creates a welcome letter for each player
 
 def create_welcome_letters(all_players, sharks, dragons, raptors):
 
 	for player in all_players:
-		name = player['name'].lower()
-		index = name.index(' ')
+		name = player['name'].lower() # name of the player 
+		index = name.index(' ') # to find the index of the space between first and last name
+		#create a file name using player's name
 		file_name = name[:index] + '_' + name[index+1:] + '.txt'
-		#print(name)
 
 		team_name = ''
 
@@ -112,10 +122,11 @@ def create_welcome_letters(all_players, sharks, dragons, raptors):
 		with open(file_name, 'w') as file_object:
 			file_object.write(letter)
 	
+#---------------------------------------------------------------------------------#
+
+if __name__ == "__main__":
+    with open('soccer_players.csv') as file_object:
+        read_csv_file(file_object) 
 
 
-
-
-
-read_csv_file() 
 
